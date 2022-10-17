61beee0393fe71588f241eee714ba258b5617282
TAHiRLI <tahrli.tahirr@gmail.com>
2022-10-17 23:05:10 +0400

    animation is added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,26 @@
+let mainDiv = document.querySelector('.main-img')
 let mainImg = document.querySelector('.main-img img');
 
 let themes = document.querySelectorAll('.theme img');
 console.log(themes);
 
+const imgScale = [
+    { transform: ' scale(0.5)' },
+    { transform: 'scale(1)' }
+  ];
+
+
+  const imgTiming = {
+    duration: 500,
+    iterations: 1,
+  }
+
+
 themes.forEach(img => {
     img.addEventListener('click', ()=>{
         mainImg.src = img.src;
+
+        mainDiv.animate(imgScale,imgTiming);
+       
     })
 })
